59475919fd69503a55b30bb556b9d4546a692c0b
Morgan Jones <mjones@morganlabs.dev>
2024-05-29 16:45:12 +0100

    style: Basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
-import { defineConfig } from "astro/config";
+import { defineConfig as define_config } from "astro/config";
+import { fileURLToPath } from "url";
+import { resolve, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const variables_scss = resolve(__dirname, "src/style/vars");
 
 // https://astro.build/config
-export default defineConfig({
+export default define_config({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "${variables_scss}" as *;`,
+        },
+      },
+    },
+  },
   redirects: {
     "/favicon.ico": "/fav/favicon.ico",
   },
